b99945b1c874b650ff75cbcbf53dfc5c241c2071
George Higbie <gehigbie@gmail.com>
2017-03-27 22:09:32 -0700

    add yargs and values for yargs

---
+++ app.js
@@ -0,0 +1,14 @@
+const yargs = require("yargs");
+
+const argv = yargs
+  .options({
+    a:{
+      demand: true,
+      alias: "address",
+      describe: "Address is required to fetch the weather.",
+      string: true
+    }
+  })
+  .help()
+  .alias("help", "h")
+  .argv;
