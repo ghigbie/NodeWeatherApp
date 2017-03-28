cf88f4824cefdac6fe17659c400a55b9c93ea4ad
George Higbie <gehigbie@gmail.com>
2017-03-28 09:55:59 -0700

    add throw new Error block

--- app.js
+++ app.js
@@ -18,7 +18,14 @@ var encodedAddress = encodeURIComponent(argv.address);
 var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
 
 axios.get(geocodeUrl).then((response) => {
+    if(response.data.status === "ZERO_RESULTS"){
+        throw new Error("Unable to find that address");
+    }
     console.log(response.data);
-}).catch((err) => {
-    console.log(err);
+}).catch((e) => {
+    if(e.code === "ENOTFOUND"){
+        console.log("Unable to connect to API servers.")
+    }else {
+        console.log(e.message);
+    }
 });
