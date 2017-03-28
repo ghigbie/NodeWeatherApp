358cb6ea78c47a373c81ef54764a8a0ccadb7f11
George Higbie <gehigbie@gmail.com>
2017-03-28 09:34:09 -0700

    change tabing

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -4,19 +4,19 @@ var geocodeAddress = (addres, callback) => {
     var encodedAddress = encodeURIComponent(address);
     request({
         url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-  		  json: true
+  		json: true
     }, (error, response, body) => ) {
         if(error){
-          callback("Unable to connect to google servers.");
+            callback("Unable to connect to google servers.");
         }else if(body.status === "ZERO_RESULTS"){
-          callback("Unable to find that addres");
+            callback("Unable to find that addres");
         }else if(body.status === "OK"){
-          callback(undefined, {
-            address: body.results[0].formatted_address,
-            latitude: body.results[0].geometry.location.lat,
-            longitude: body.results[0].geometry.location.lng
-          });
-        }
+            callback(undefined, {
+                address: body.results[0].formatted_address,
+                latitude: body.results[0].geometry.location.lat,
+                longitude: body.results[0].geometry.location.lng
+              });
+          }
       });
 };
 
